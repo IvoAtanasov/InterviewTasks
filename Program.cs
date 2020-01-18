466e8a388bbc7cfbc381d1547a5557b86514addf
IvoAtanasov <ivoatanasov85@gmail.com>
2020-01-18 16:07:15 +0200

    finish task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InterviewTasks.Task18_01_2020;
+using System;
 
 namespace InterviewTasks
 {
@@ -6,7 +7,22 @@ namespace InterviewTasks
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            CallSumBinary();
+        }
+
+        private static void CallSumBinary()
+        {
+            string first = string.Empty;
+            string second = string.Empty;
+            Console.WriteLine("Enter First Number:");
+            first=Console.ReadLine();
+            Console.WriteLine("Enter Second Number:");
+            second = Console.ReadLine();
+
+            SumBinary task = new SumBinary(first, second);
+
+            Console.WriteLine(String.Format("Result is {0}", task.ReturnResult()));
         }
     }
 }
--- Task18_01_2020/SumBinary.cs
+++ Task18_01_2020/SumBinary.cs
@@ -33,11 +33,28 @@ namespace InterviewTasks.Task18_01_2020
                 loopEnd = secondLenght;
                 FirstNumber = LeadingZeros(secondLenght - firstLenght) + FirstNumber;
             }
-            for (int i = 0; i < loopEnd; i++)
+            for (int i = loopEnd-1; i >= 0; i--)
             {
-
+                int currentSum = int.Parse(FirstNumber[i].ToString()) + int.Parse(SecondNumber[i].ToString());
+                if (oneInMind)
+                {
+                    currentSum += 1;
+                }
+                oneInMind = false;
+                switch (currentSum)
+                {
+                    case 0: result.Append("0"); break;
+                    case 1:result.Append("1");break;
+                    case 2: result.Append("0"); oneInMind = true; break;
+                    case 3: result.Append("1"); oneInMind = true;break;
+                    default: throw new ArgumentException("Inccorrect operation");
+                }
             }
-            return result.ToString();
+            if (oneInMind)
+            {
+                result.Append("1");
+            }
+            return ReverseString( result.ToString());
         }
 
         private string LeadingZeros(int numberOfZeos)
@@ -46,6 +63,17 @@ namespace InterviewTasks.Task18_01_2020
             for (int i = 0; i < numberOfZeos; i++)
             {
                 result.Append("0");
+            }
+            return result.ToString();
+        }
+
+        private string ReverseString(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            int strlen = input.Length;
+            for (int i = 0; i < strlen; i++)
+            {
+                result.Append(input[strlen - 1 - i]);
             }
             return result.ToString();
         }
