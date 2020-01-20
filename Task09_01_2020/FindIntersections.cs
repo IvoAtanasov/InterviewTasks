531ecb2ca8e9a3d76709099e4028bfbac2d2f3b8
IvoAtanasov <ivoatanasov85@gmail.com>
2020-01-20 23:47:51 +0200

    addstatic method to find intersection

--- Task09_01_2020/FindIntersections.cs
+++ Task09_01_2020/FindIntersections.cs
@@ -6,6 +6,29 @@ namespace InterviewTasks.Task09_01_2020
 {
     public static class FindIntersections
     {
-
+        public static int FindTreeSortArrayIntersection(int[] arr1, int[] arr2, int[] arr3)
+        {
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int y = 0; y < arr2.Length; y++)
+                {
+                    if (arr1[i] < arr2[y])
+                    {
+                        break;
+                    }
+                    if (arr1[i] == arr2[y])
+                    {
+                        for (int z = 0; z < arr3.Length; z++)
+                        {
+                            if (arr1[i] == arr3[z])
+                            {
+                                return arr3[z];
+                            }
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
     }
 }
