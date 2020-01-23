ddac172f26ca7f54ce83615b5b14d2d33dcb2ace
IvoAtanasov <ivoatanasov85@gmail.com>
2020-01-23 20:31:39 +0200

    add dictionary

--- Task21_01_2020/RomanNumber.cs
+++ Task21_01_2020/RomanNumber.cs
@@ -6,6 +6,16 @@ namespace InterviewTasks.Task21_01_2020
 {
    public static class RomanNumber
     {
+        private Dictionary<string, int> MapNumbers = new Dictionary<string, int>()
+        {
+            {"I",1},
+            {"V",5},
+            {"X",10},
+            {"L",50},
+            {"C",100},
+            {"D",500},
+            {"M",1000}
+        };
         public static string ConvetToRoman(int input)
         {
             StringBuilder result = new StringBuilder();
