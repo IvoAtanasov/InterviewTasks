56b8c3bdbb38938b8e5152b87102a44f2f708ad6
IvoAtanasov <ivoatanasov85@gmail.com>
2020-01-23 20:25:23 +0200

    add folder and a class

---
+++ Task21_01_2020/RomanNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewTasks.Task21_01_2020
+{
+   public static class RomanNumber
+    {
+        public static string ConvetToRoman(int input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            return result.ToString();
+        }
+    }
+}
